8cc31a9e6c44552ff027ad70a2b16d650a558e9d
yachun0728 <yachun0728@gmail.com>
2016-04-11 19:19:09 +0800

    first commit

---
+++ Homework/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using Homework.Models;
+
+namespace Homework.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult List()
+        {
+            var moneyList = new List<MoneyList>();
+            moneyList.Add(new MoneyList()
+            {
+                Category="支出",
+                Money=(decimal)1000,
+                Date=DateTime.Now.Date,
+                Description = "買書"
+            });
+
+            moneyList.Add(new MoneyList()
+            {
+                Category = "支出",
+                Money = (decimal)1500,
+                Date = DateTime.Now.Date,
+                Description = "買文具"
+            });
+
+            moneyList.Add(new MoneyList()
+            {
+                Category = "收入",
+                Money = (decimal)300,
+                Date = DateTime.Now.Date,
+                Description = "賣筆"
+            });
+
+            return View(moneyList);
+        }
+    }
+}
---
+++ Homework/Models/MoneyList.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Homework.Models
+{
+    public class MoneyList
+    {
+        public string Category { get; set; }
+        public decimal Money { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+    }
+}
