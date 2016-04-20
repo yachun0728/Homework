90396c82e830beeccfc0d93e77aebe6d7cf59600
yachun0728 <yachun0728@gmail.com>
2016-04-20 12:57:12 +0800

    Change Date and Money's format

--- Homework/Models/MoneyList.cs
+++ Homework/Models/MoneyList.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using Homework.Enums;
 
 namespace Homework.Models
@@ -6,7 +7,11 @@ namespace Homework.Models
     public class MoneyList
     {
         public string Category { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:N0}")]
         public decimal Money { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
         public DateTime Date { get; set; }
         public string Description { get; set; }
         public int Number { get; set; }
